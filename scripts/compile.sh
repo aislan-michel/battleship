31dd43f3c5d58c640f4a6f828af1d2358563a4c6
Aislan Michel Moreira Freitas <aislan.michel92@gmail.com>
2026-05-17 17:48:34 -0300

    src folder

---
+++ scripts/compile.sh
@@ -0,0 +1,6 @@
+g++ -g src/main.cpp \
+  battleship/Models/Grid/Grid.cpp \
+  battleship/Models/Player/Player.cpp \
+  battleship/Models/Position/Position.cpp \
+  battleship/Models/Ship/Ship.cpp \
+  -o battleship/battleship.exe
